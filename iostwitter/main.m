94bf8eb6d170786b8018aa14891b397cfca3db34
natarajk <natarajk@yahoo-inc.com>
2014-07-02 22:30:10 -0700

    Initial Commit

---
+++ iostwitter/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  iostwitter
+//
+//  Created by Natarajan Kannan on 7/2/14.
+//  Copyright (c) 2014 Y.CORP.YAHOO.COM\natarajk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
